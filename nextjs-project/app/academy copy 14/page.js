f4a32388bbff794a95e333be67ec002cdc01552f
TanujKumar41 <tanujkumar191001@gmail.com>
2025-07-06 13:41:33 +0530

    changes

---
+++ nextjs-project/app/academy copy 14/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AcademySimulator = () => {
+    return (
+        <div>
+            <h1>Academy Simulator</h1>
+            <p>Welcome to the Academy Simulator page!</p>
+        </div>
+    );
+};
+
+export default AcademySimulator;
--- nextjs-project/app/pages/classroom-studies/page.js
+++
@@ -1,8 +0,0 @@
-export default function ClassroomStudies() {
-    return (
-        <div>
-            <h1>Welcome to Classroom Studies</h1>
-            <p>This is a sample page for classroom studies.</p>
-        </div>
-    );
-}
