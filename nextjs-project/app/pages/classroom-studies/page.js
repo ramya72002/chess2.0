cf23e5d5f40f2d09e1ece13a37406459ea9b2dfe
ramya <nsriramya7@gmailcom>
2025-07-06 12:27:58 +0530

    base setup

---
+++ nextjs-project/app/pages/classroom-studies/page.js
@@ -0,0 +1,8 @@
+export default function ClassroomStudies() {
+    return (
+        <div>
+            <h1>Welcome to Classroom Studies</h1>
+            <p>This is a sample page for classroom studies.</p>
+        </div>
+    );
+}
